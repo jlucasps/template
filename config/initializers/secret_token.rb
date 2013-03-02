68bcbba40a49e74e89aa330e9a2dcd05edd9f375
João Lucas <jlucasps@gmail.com>
2013-03-01 23:12:28 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoptec::Application.config.secret_token = '7862c50f464700b519be5a96cbd3056e5d8aede0423b3410f0fd5a5e4e42690ca5e967115a517882868747c02d84b70cd175c834096e0cbbb8fe7d57f41e73b8'
